ce46f03b3586a068d380f4970c0f8f533ab9ec35
OlegUkhov <o_ukhov@kbtu.kz>
2025-01-22 08:30:59 +0500

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,128 @@
+#STRINGS
+
+print("Hello")
+print('Hello')
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a = "Hello"
+print(a)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a) 
+
+a = "Hello, World!"
+print(a[2])
+
+for x in "banana":
+    print(x) 
+
+a = "Hello, World!"
+print(len(a))
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+    print("Yes, 'free' is present.")
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+    print("No, 'expensive' is NOT present.")
+
+a = "Hello World"
+for i in a:
+    print(i)
+
+#SLICING
+
+b = "Hello, World!"
+print(b[2:5])
+
+b = "Hello, World!"
+print(b[:5])
+
+b = "Hello, World!"
+print(b[2:])
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+#MODIFY STRINGS
+
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!'] 
+
+print(a.split('l'))
+
+#CONCATENATE
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+print(a + c + c + a)
+
+#FORMAT STRINGS
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+
+#ESCAPE CHARACTERS
+
+txt = "We are the so-called \"Vikings\" from the north." 
+
+#METHODS
+a = 'hello'
+print(a.capitalize())
+
+b = 35
+b = format(b)
+print(type(b))
+
+c = "hello world hello"
+print(c.title(), c.title().swapcase())
